007dca8c6f82cca95e58084f6d6e6b4ce226d7c0
amitsinghlabana <amitsingh7038@gmail.com>
2022-02-08 02:47:25 +0530

    next_auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_SECRET,
       }
     ),   
 // ...add more providers here
